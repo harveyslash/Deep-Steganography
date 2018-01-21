92ef7ce4b29f6f7d65f8bc26eb769540687e0b1b
harsh <harshvardhan_gupta@yahoo.com>
2018-01-20 19:29:25 -0500

    setup for valohai

--- src/Models/SingleSizeModel.py
+++ src/Models/SingleSizeModel.py
@@ -242,9 +242,13 @@ class SingleSizeModel():
                 
             
         
-m = SingleSizeModel(beta=.75,log_path="/valohai/outputs/")
+m = SingleSizeModel(beta=.25,log_path="/valohai/outputs/")
 # m.load_chkp("/home/harsh/ml/Stegano/checkpoints/beta_0.75.chkp-102192")
 files_list = glob.glob("/valohai/inputs/training-set-images/train/"+"**/*")
-m.train(100,files_list,8)
+print(files_list)
+for i in range(100):
+    m.make_chkp('/valohai/outputs/beta_.25.chkp')
+    m.train(1000,files_list,32)
+
 # print(files_list)
 
